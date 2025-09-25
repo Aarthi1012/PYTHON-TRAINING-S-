d31a2e01e9fbcef329ee41dd62d3053835485461
Aarthi1012 <aarthivenkat04@gmail.com>
2025-09-25 11:31:55 +0530

    25/9/25

---
+++ amt.py
@@ -0,0 +1,10 @@
+correct_password = "123"
+password = input("Enter your password: ")
+
+while password != correct_password:
+    print("Incorrect password")
+    password = input("Enter your password: ")
+
+print("Password is correct")
+print("login successfuly.")
+print("thankyou")
